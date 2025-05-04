9118928e78858d6597d263d43ae2e16a088e72b2
ayesha <ayeshask421@gmail.com>
2025-05-04 14:02:39 +0530

    day-14-workspaces

---
+++ day-14-workspaces/main.tf
@@ -0,0 +1,10 @@
+
+
+resource "aws_instance" "example" {
+  ami           = "ami-062f0cc54dbfd8ef1"  
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "Instance-${terraform.workspace}"
+  }
+}
